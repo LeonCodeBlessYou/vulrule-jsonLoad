a6009cf61c31f55b47971fe04fc4782fb78306d1
Luyang Yu <13806801+luyang-yu@user.noreply.gitee.com>
2025-06-30 22:33:02 +0800

    yly@202506302233

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: 'https://github.com/LeonCodeBlessYou/vulrule-jsonLoad', // 替换成你的仓库名
+    output: 'static',
+    base: '/vulrule-jsonLoad/',
+    site: 'https://leoncodeblessyou.github.io/vulrule-jsonLoad/'
 });
